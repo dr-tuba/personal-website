6be4c5c9f56d75b7e509c7d1ee231deed8fcb713
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-05-21 20:03:40 -0600

    update

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+let aboutImg1 = document.getElementsByClassName("about-img")[0];
+let aboutImg2 = document.getElementsByClassName("about-img")[1];
+let aboutImg3 = document.getElementsByClassName("about-img")[2];
+let aboutImg4 = document.getElementsByClassName("about-img")[3];
+
+aboutImg1.addEventListener("click", function() {
+    aboutImg1.classList.remove("animate__zoomInLeft")
+    aboutImg1.classList.toggle("animate__tada")
+})
+
+aboutImg2.addEventListener("click", function() {
+    aboutImg2.classList.remove("animate__zoomInRight")
+    aboutImg2.classList.toggle("animate__tada")
+})
+
+aboutImg3.addEventListener("click", function() {
+    aboutImg3.classList.remove("animate__zoomInLeft")
+    aboutImg3.classList.toggle("animate__tada")
+})
+
+aboutImg4.addEventListener("click", function() {
+    aboutImg4.classList.remove("animate__zoomInRight")
+    aboutImg4.classList.toggle("animate__tada")
+})
 
 
